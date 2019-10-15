ef9a09cff9ecd6c9bc6d115b98db56da0f9f81f4
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-15 13:26:50 -0600

    got tests working, dependencies are updated

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard.js';
+
 // Test away
+test('Dashboard renders correctly', () => {
+  render (<Dashboard />)
+})
